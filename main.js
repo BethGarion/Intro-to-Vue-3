3bfb71709f19ce965b36cd63a14f0a0fe9ffe1a4
BethGarion <tperrault.ing2024@esaip.org>
2022-11-05 17:43:23 +0100

    Exercice 1

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Boots',
+            description: 'Leather Boots'
+        }
+    }
+})
+
+const app2 = Vue.createApp({
+    data() {
+        return {
+            description: 'Leather Boots'
+        }
+    }
+})
